eb865daa4aee51126b8979e7cdbcb91919ea260b
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-10-07 18:46:53 +0300

    ui now updates correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
 //Selecting DOM Elements
 const diceImages = document.querySelectorAll("form img");
-const newGameButton = document.querySelector("#new-game-btn");
-const rollDiceButton = document.querySelector("#roll-dice-btn");
-const holdButton = document.querySelector("#hold-btn");
 const buttonsForm = document.querySelector("form");
+const player1Section = document.querySelector("#player1-section");
+const player2Section = document.querySelector("#player2-section");
+const currentScoreHeaders = document.querySelectorAll(".current-score-section h2");
+const totalScoreHeaders = document.querySelectorAll(".total-score");
+const messages = document.querySelectorAll(".message");
+
+let currentPlayerIndex = 0;
 
 // Add event listeners to buttons
 buttonsForm.addEventListener("click", gameButtonsListener);
 
-function gameButtonsListener(event){
+function gameButtonsListener(event) {
   event.preventDefault();
 
-  const targetId = event.target.id
+  const targetId = event.target.id;
 
   switch (targetId) {
     case "new-game-btn":
@@ -24,8 +28,7 @@ function gameButtonsListener(event){
       hold();
       break;
     default:
-      console.log(targetId);
-      break;
+      return;
   }
 }
 
@@ -34,71 +37,107 @@ const player1 = {
   totalScore: 0,
   currentScore: 0,
   totalNumOfWins: 0,
-}
+};
 const player2 = {
   playerName: "Player 2",
   totalScore: 0,
   currentScore: 0,
   totalNumOfWins: 0,
-}
+};
 
-let currentPlayer = null;
+const players = [player1, player2];
 
-startNewGame();
+// startNewGame();
 
-function startNewGame(){
+function startNewGame() {
   initializeValues();
 
-  currentPlayer = player1;
-  console.log(`Game Started! Current Player: ${currentPlayer.playerName}`);
+  currentPlayerIndex = 0;
+
+  player1Section.classList.add("current-player-section");
+  player2Section.classList.remove("current-player-section");
 }
 
-function initializeValues(){
+function initializeValues() {
   player1.totalScore = 0;
   player1.currentScore = 0;
   player2.totalScore = 0;
   player2.currentScore = 0;
+
+  currentPlayerIndex = 0;
+
+  //reset current score headers
+  currentScoreHeaders.forEach((header) => {
+    header.innerText = "0";
+  });
+
+  //reset dice images
+  diceImages.forEach((image) => {
+    image.src = `./assets/imgs/dice pics/dice-1.png`;
+  });
+
+  // reset totalScoreHeaders
+  totalScoreHeaders.forEach((header) => {
+    header.innerText = "0";
+  });
 }
 
-function endTurn(){
-  //switch current player
-  console.log(currentPlayer.playerName, player1.playerName);
-  if(currentPlayer === player1){
-    currentPlayer = player2;
+function endTurn() {
+  if (currentPlayerIndex === 0) {
+    currentPlayerIndex = 1;
+    player1Section.classList.remove("current-player-section");
+    player2Section.classList.add("current-player-section");
   } else {
-    currentPlayer = player1;
+    currentPlayerIndex = 0;
+    player1Section.classList.add("current-player-section");
+    player2Section.classList.remove("current-player-section");
   }
-  console.log(`turn ended! Current Player: ${currentPlayer.playerName}`);
 }
 
 // saves current score in total score, resets current score and ends turn
-function hold(){
-  currentPlayer.totalScore += currentPlayer.currentScore;
-  currentPlayer.currentScore = 0;
+function hold() {
+  // adds current score to total score
+  players[currentPlayerIndex].totalScore += players[currentPlayerIndex].currentScore;
+  // resets current score
+  players[currentPlayerIndex].currentScore = 0;
+
+  updateUI();
 
   endTurn();
 }
 
 // Simulates the dice rolls
-function rollDice(){
-  if(currentPlayer){
+function rollDice() {
     // generate a random number between 1 and 6 for each dice
     let dice1 = Math.floor(Math.random() * 6) + 1;
-    diceImages[0].src = `./assets/imgs/dice pics/dice-${dice1}.png`
-    console.log('dice1', dice1)
+    diceImages[0].src = `./assets/imgs/dice pics/dice-${dice1}.png`;
+    console.log("dice1", dice1);
     let dice2 = Math.floor(Math.random() * 6) + 1;
-    diceImages[1].src = `./assets/imgs/dice pics/dice-${dice2}.png`
-    console.log('dice2', dice2)
+    diceImages[1].src = `./assets/imgs/dice pics/dice-${dice2}.png`;
+    console.log("dice2", dice2);
 
     // check if both values are not equal then add to currentScore
-    if(dice1 != dice2){
-      currentPlayer.currentScore += (dice1 + dice2);
-    } else { // if they're equal reset current score and end turn
+    if (dice1 != dice2) {
+      players[currentPlayerIndex].currentScore += dice1 + dice2;
+      updateUI();
+    } else {
+      // if they're equal reset current score and end turn
       console.log("ROLLED A DOUBLE");
-      currentPlayer.currentScore = 0;
+
+
+      // players[currentPlayerIndex].currentScore += dice1 + dice2;
+      players[currentPlayerIndex].currentScore = 0;
+      updateUI();
+
       //end turn
       endTurn();
     }
-    console.log(currentPlayer);
-  }
+    // console.log(currentPlayer);
+  
+}
+
+// if player index is 0 it updates player 1's innerText if the index is 1 it updates player 2's innerText
+function updateUI() {
+  currentScoreHeaders[currentPlayerIndex].innerText = players[currentPlayerIndex].currentScore;
+  totalScoreHeaders[currentPlayerIndex].innerText = players[currentPlayerIndex].totalScore;
 }
